aa618ae93fc060e7c881713320b9782f68c7afad
sara duell <116595087+sara-duell@users.noreply.github.com>
2023-07-14 15:31:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -53,12 +53,7 @@ console.log( "ready!" );
                       console.log(invitationResponse)
                       $( ".my-fun-class" ).append( "<p>"+ invitationResponse +"</p>" );
                   }
-                  for (var entry of data) {
-                      //note that it's not as simple to address this value becuase the name of it is not very machine readable, unlike "Timestamp." The name of it is "What ways do you like to invite someone to do something with you?." This can't be used in the same way becuase of all the spaces and punctuation so we need to address it by referring to is as the second key of each object (remember second looks like [1]) for each entry object.
-                      var number2Responses = entry[Object.keys(entry)[2]]
-                      console.log(number2Responses)
-                      $( ".my-fun-class-2" ).append( "<p>"+ number2Responses +"</p>" );
-                  }
+
 
                   //// This is the original table code that displays the data similar to the way the spreadsheet looks using a table library
                   // var options = {
