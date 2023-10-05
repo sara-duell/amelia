d3bbc4fd4568e1c0d9c64c07ea4ab445f0fe558a
sara duell <116595087+sara-duell@users.noreply.github.com>
2023-10-05 19:18:26 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,29 +2,27 @@ let img01, img02;
 let topLayer;
 
 function preload() {
-    img01 = loadImage("sorrel1.jpg");
-    img02 = loadImage("sorrel2.jpg");
+    img01 = loadImage("mimiBB1A.jpg");
+    img02 = loadImage("mimiBB1B.jpg");
 }
 
 function setup() {
-	createCanvas(1440, 960);  
-    topLayer = createGraphics(1440, 960);
+  createCanvas(2400, 1600);
+  topLayer = createGraphics(2400, 1600)
   
-  topLayer.image(img01, 0, 0, 1440, 960);
+  topLayer.image(img01, 0, 0, 2400, 1600)
   
-  topLayer.strokeWeight(300);
-  topLayer.blendMode(REMOVE);
+  topLayer.strokeWeight(100);
+  topLayer.blendMode(REMOVE)
 }
 
-
 function draw() {
-  image(img02, 0, 0, 1440, 960);
+  image(img02, 0, 0, 2400, 1600);
 
   if(mouseIsPressed) {
     topLayer.line(pmouseX, pmouseY, mouseX, mouseY);
   }
   
-  image(topLayer, 0, 0);
+  image(topLayer, 0, 0)
 }
 
-}
