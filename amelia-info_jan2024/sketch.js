bbdec20788acebe47d901e2ffe62ae4170cb5996
sara duell <116595087+sara-duell@users.noreply.github.com>
2024-01-09 15:16:33 -0500

    Update sketch.js

--- amelia-info_jan2024/sketch.js
+++ amelia-info_jan2024/sketch.js
@@ -1,13 +1,4 @@
-var txt;
-function preload() {
-    txt = loadStrings("writings.txt");
-  }
 
-  function setup () {
-noCanvas();
-    // console.log(txt);
-createP(join (txt, "<br/>")).parent('writing');
-  }
 
 let img01, img02;
 let topLayer;
