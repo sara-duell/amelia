5c018a3ff5f3e14000550023f6478214b23b1b3e
sashaplot <143138088+sashaplot@users.noreply.github.com>
2024-01-09 15:32:46 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,14 +10,14 @@ function setup() {
   createCanvas(displayWidth, displayHeight);
   topLayer = createGraphics(displayWidth, displayHeight)
   
-  topLayer.image(img01, 0, 0, 200, 133)
+  topLayer.image(img01, 0, 0, 2000, 1333)
   
   topLayer.strokeWeight(300);
   topLayer.blendMode(REMOVE)
 }
 
 function draw() {
-  image(img02, 0, 0, 200, 133);
+  image(img02, 0, 0, 2000, 1333);
 
   if(mouseIsPressed) {
     topLayer.line(pmouseX, pmouseY, mouseX, mouseY);
