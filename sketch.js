ee8deb1edb7ee4c51e631b0de59406dca9f8dada
sashaplot <143138088+sashaplot@users.noreply.github.com>
2024-01-12 14:21:46 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let img01, img02;
 let topLayer;
+var txt;
 
 function preload() {
     img01 = loadImage("fog1.jpg");
     img02 = loadImage("fog2a.jpg");
+    txt = loadStrings ("bio.txt");
 }
 
 function setup() {
@@ -11,9 +13,11 @@ function setup() {
   topLayer = createGraphics(displayWidth, displayHeight)
   
   topLayer.image(img01, 0, 0, displayWidth, displayHeight)
-  
   topLayer.strokeWeight(100);
   topLayer.blendMode(REMOVE)
+
+    console.log(txt);
+createP(join (txt, "<br/>")).parent('intro');
 }
 
 function draw() {
@@ -25,3 +29,12 @@ function draw() {
   
   image(topLayer, 0, 0)
 }
+
+// function createMetaTag() {
+// 	let meta = createElement('meta');
+// 	meta.attribute('name', 'viewport');
+// 	meta.attribute('content', 'user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width,height=device-height');
+
+// 	let head = select('head');
+// 	meta.parent(head);
+// }
