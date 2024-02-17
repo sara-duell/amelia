d5f742c2d9871101e7c6327ea7c216ab6b3d242a
sara duell <116595087+sara-duell@users.noreply.github.com>
2024-02-17 11:07:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+let img01, img02;
+let topLayer;
+var txt;
+
+
+function preload() {
+    img01 = loadImage('images/fog1night.jpg');
+    img02 = loadImage('images/fog2night.jpg');
+     txt = loadStrings("writings.txt");
+}
+
+function setup() {
+  //createMetaTag();
+  createCanvas(displayWidth, displayHeight);
+  topLayer = createGraphics(displayWidth, displayHeight)
+  
+  topLayer.image(img01, 0, 0, 1920, 1280, 0, 0, 1920, 1280, CONTAIN)
+  topLayer.strokeWeight(100);
+  topLayer.blendMode(REMOVE)
+ 
+    console.log(txt);
+createP(join (txt, "")).parent('intro');
+}
+
+function draw() {
+  image(img02, 0, 0, 1920, 1280, 0, 0, 1920, 1280, CONTAIN);
+
+  if(mouseIsPressed) {
+    topLayer.line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+  
+  image(topLayer, 0, 0)
+}
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+// function createMetaTag() {
+// 	let meta = createElement('meta');
+// 	meta.attribute('name', 'viewport');
+// 	meta.attribute('content', 'user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width,height=device-height');
+
+// 	let head = select('head');
+// 	meta.parent(head);
+// }
+
+
+
